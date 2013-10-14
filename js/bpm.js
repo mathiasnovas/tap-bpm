88aec62cb06d8a05437ee47f16bed40098a4d636
Mathias Novas <mathias@07.no>
2013-10-14 13:38:23 +0200

    Add styles and restructure code

--- js/bpm.js
+++ js/bpm.js
@@ -1,26 +1,20 @@
 jQuery(function ($) {
-
     var lastClick = 0;
-        numbs = [],
         count = 0,
-        pool = 0;
-    
+        canvas = $('#canvas');
+
     $(document).on('click', function () {
-        
         var timeNow = (new Date()).getTime();
 
         if (count > 0) {
             var diff = (timeNow - lastClick) / 1000,
                 hardBPM = 60/diff;
 
-            numbs.push(hardBPM);
-
-            console.log(hardBPM);
+            canvas.html(Math.round(hardBPM));
         }
 
         lastClick = timeNow;
         count ++;
-
     });
 });
 
