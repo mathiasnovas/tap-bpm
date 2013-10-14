f0c3d4f33bb9b0494b2907cfbed607bb3afbb498
Mathias Novas <mathias@07.no>
2013-10-14 14:51:23 +0200

    Add comments

--- js/bpm.js
+++ js/bpm.js
@@ -1,32 +1,47 @@
 jQuery(function ($) {
-    var lastClick = 0;
+
+    /**
+     * Assign variables.
+     */
+    var lastClick = 0,
+        doc = $(document),
         count = 0,
         canvas = $('#canvas'),
         pool = [];
 
-    $(document).on('click', function () {
+    /**
+     * Start the BPM mapping when clicking the document.
+     *
+     * It will always be 0 the first time you click as two values
+     * are necessary to calculate the BPM.
+     */
+    doc.on('click', function () {
         var timeNow = (new Date()).getTime(),
             sum = 0,
             bpm = 0;
 
+        // We need at least two values to compare.
         if (count > 0) {
             var diff = (timeNow - lastClick) / 1000,
                 hardBpm = 60 / diff;
 
             pool.push(hardBpm);
 
+            // Calculate the average BPM
             for (var i = 0; i < pool.length; i++) {
                 sum += pool[i];
             }
 
             bpm = Math.round(sum / pool.length);
 
+            // Empty the value pool every 10th click.
             if (count === 10) {
                 pool = [];
                 count = 1;
             }
         }
 
+        // Display the BPM
         canvas.html(bpm);
 
         lastClick = timeNow;
